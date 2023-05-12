e07d2cf2c499712ba9efa3db88b02c8583cd575a
sparkcodes <122539340+sparkie11@users.noreply.github.com>
2023-05-12 11:35:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import Ben from "./Components/Ben";
+import Boxone from "./Components/Boxone";
+import Boxx from "./Components/Boxx";
+import Filter from "./Components/Filter";
+import Gwen from "./Components/Gwen";
+import Jeba from "./Components/Jeba";
+import Reddy from "./Components/Reddy";
+import Rest from "./Components/Rest";
+import Search from "./Components/Search";
+import Face from "./Face";
+import { Counter } from "./features/counter/Counter";
+
+// import Task from "./Components/Task";
+// import One from "./Components/one";
+// import Rest from "./Components/Rest";
+
+function App() {
+  return (
+    <div className="App">
+      <Reddy number={1} />
+      <Reddy number={2} />
+      <Reddy number={3} />
+      <Reddy number={4} />
+      <Reddy number={5} />
+      <Boxone />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
